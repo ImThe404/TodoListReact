1865a393cb7f9fed5ed9da1483caec701c1ec7c8
Jonathan Aussant <aussant222@CAMPUS@V304V-JCP052.campus.unicaen.fr>
2024-10-22 14:24:19 +0200

    je vous emmerde

--- Screen/TodoListDetailsScreen.js
+++ Screen/TodoListDetailsScreen.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 
 import TodoList from '../components/TodoListUI';
+import { getTodos } from "../components/todo"
+import { TokenContext, UsernameContext } from '../Contexte/Context'
 
 export default function TodoListScreen(props) {
+
+    console.log(JSON.stringify(props))
+    console.log(props.route.params.id)
+
+    const [data, setData] = useState(null);
+
+     // Créer une fonction asynchrone
+    useEffect (() => {
+        const Todos = async () => {
+            setData( await getTodos(props.route.params.id, TokenContext) )
+        };
+
+        Todos();
+    }, []);
+
     return (
         <View>
             <TodoList
-                data={props.data}
+                data={data}
             />
         </View>
     );
